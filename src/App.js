8170f462640a497da4408622e16444269418ab02
Jonah-Hansen <tce_mc@telus.net>
2022-11-30 18:59:40 -0600

    connect supabase and test functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { createClient } from '@supabase/supabase-js';
+import { useEffect } from 'react';
 import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
+  const { REACT_APP_API_URL: api_url, REACT_APP_API_KEY: api_key } = process.env
+  const supabase = createClient(api_url, api_key)
+
+  useEffect(() => {
+    const getTracks = async () => {
+      let { data: tracks } = await supabase.from('tracks').select('*')
+      console.log(tracks);
+    }
+    getTracks()
+  }, [supabase])
+
   return (
     <BrowserRouter>
       <Routes>
