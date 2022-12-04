debabdfdf86a05edaea883f2842694cb6dc4f678
Jonah-Hansen <tce_mc@telus.net>
2022-12-04 15:46:26 -0600

    create ruler canvas

--- src/components/Player/PlayerRuler/PlayerRuler.js
+++ src/components/Player/PlayerRuler/PlayerRuler.js
@@ -1,30 +1,11 @@
-import { height } from '@mui/system';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import './PlayerRuler.scss';
 
 export default function PlayerRuler() {
 
   const canvasRef = useRef()
 
-  const draw = () => {
-    const canvas = canvasRef.current;
-
-    const height = 32 * window.devicePixelRatio
-
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
-
-      ctx.strokeStyle = '#697085'
-      ctx.lineWidth = 1
-
-    }
-  }
-
-  useEffect(() => {
-    draw()
-  }, [])
-
   return (
-    <canvas ref={canvasRef} className='player-ruler' />
+    <canvas ref={canvasRef} className='player-ruler' height='32' />
   )
 }
