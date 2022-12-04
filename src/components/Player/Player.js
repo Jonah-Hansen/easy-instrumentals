9bd49bcb3c5cb4815614b88de2fab044dc6052ab
Jonah-Hansen <tce_mc@telus.net>
2022-12-03 23:27:12 -0600

    add state to handle swapping tabs

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import PlayerHeader from './PlayerHeader/PlayerHeader'
 
 export default function Player() {
+
+  const [activeTab, setActiveTab] = useState('arrange')
+
   return (
     <div>
-      <PlayerHeader />
+      <PlayerHeader tabState={{ activeTab, setActiveTab }} />
       {/* view */}
       {/* controls */}
     </div>
--- src/components/Player/PlayerHeader/PlayerHeader.js
+++ src/components/Player/PlayerHeader/PlayerHeader.js
@@ -3,15 +3,29 @@ import { ReactComponent as ArrangeIcon } from '../../../assets/images/icons/arra
 import { ReactComponent as PianoIcon } from '../../../assets/images/icons/piano.svg'
 import './PlayerHeader.scss'
 
-export default function PlayerHeader() {
+export default function PlayerHeader({ tabState }) {
+
+  const { activeTab, setActiveTab } = tabState
+
   return (
     <header className="player-header">
       <nav className="player-header__nav">
-        <div className="player-header__tab active">
+        <div
+          onClick={() => setActiveTab('arrange')}
+          className={`player-header__tab
+            ${activeTab === 'arrange' ? ' active' : ''}`
+          }
+        >
           <ArrangeIcon className="player-header__icon" />
           <span className="player-header__label">Arrange</span>
         </div>
-        <div className="player-header__tab">
+
+        <div
+          onClick={() => setActiveTab('piano')}
+          className={`player-header__tab
+            ${activeTab === 'piano' ? ' active' : ''}`
+          }
+        >
           <PianoIcon className="player-header__icon" />
           <span className="player-header__label">Piano Roll</span>
         </div>
