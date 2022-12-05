b3e08e350ea3321f8e9e7be83888d3b3c67b3a29
Jonah-Hansen <tce_mc@telus.net>
2022-12-04 22:46:22 -0600

    implement drag and drop with react-dnd

--- src/components/Browser/BrowserCategory/BrowserCategory.js
+++ src/components/Browser/BrowserCategory/BrowserCategory.js
@@ -1,6 +1,7 @@
-import { PlayArrow } from '@mui/icons-material'
+
 import { useEffect, useState } from 'react'
 import { getTracksByType } from '../../../supabase/supabase'
+import BrowserTrack from '../BrowserTrack/BrowserTrack'
 import './BrowserCategory.scss'
 
 export default function BrowserCategory({ title }) {
@@ -15,12 +16,9 @@ export default function BrowserCategory({ title }) {
   return (
     <section className='browser-category'>
       <h3 className='browser-category__heading'>{title}</h3>
-      <ul className='browser-catergory__track-list'>
+      <ul className='browser-category__track-list'>
         {tracks.map(track =>
-          <li key={track.id} className='browser-category__track' >
-            <PlayArrow />
-            <span>{track.title}</span>
-          </li>
+          <BrowserTrack key={track.id} track={track} type={title.toLowerCase()} />
         )}
       </ul>
     </section>
--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -1,13 +1,17 @@
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import Browser from "../components/Browser/Browser"
 import Player from "../components/Player/Player"
 
 function CreatePage() {
 
   return (
-    <main>
-      <Player />
-      <Browser />
-    </main>
+    <DndProvider backend={HTML5Backend}>
+      <main>
+        <Player />
+        <Browser />
+      </main>
+    </DndProvider>
   )
 }
 
